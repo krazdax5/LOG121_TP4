38527f16b44181e36eab5bdd099a84952d3e9d86
Pierre-Luc Landry <pl.landry.13@gmail.com>
2013-11-21 17:31:10 -0500

    Réalisation de la classe VueRéduite

--- LOG121_TP4/src/ets/VueReduite.java
+++ LOG121_TP4/src/ets/VueReduite.java
@@ -1,5 +1,9 @@
 package ets;
 
+import java.awt.*;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+
 /**
  * Patron : Immutable
  *
@@ -8,5 +12,29 @@ package ets;
  * @author
  * 2013-11-
  */
-public class VueReduite {
+public class VueReduite  {
+
+    /**
+     * Constructeur qui permet de passer en parametre l'image que l'on veut changer,
+     * avec les dimensions desirees.
+     * @param image L'image que l'on veut reduire
+     * @param largeur Largeur que l'on veut attribuer a l'image
+     * @param longueur Longueur que l'on veut attribuer a l'image
+     */
+    public VueReduite (Image image, int largeur, int longueur){
+
+        BufferedImage buf = new BufferedImage(largeur,longueur, BufferedImage.TYPE_INT_ARGB);
+
+        Graphics2D g = buf.createGraphics();
+
+        g.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+        g.drawImage(image,0,0,largeur,longueur,null);
+        g.dispose();
+
+
+    }
+
+
+    
+
 }
