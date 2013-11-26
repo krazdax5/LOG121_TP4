c5accc643a4cf0c4455f9d9e5491411fa94b6f0e
Jacob Marcil <jacobmarcil2@gmail.com>
2013-11-26 15:50:41 -0500

    Perspective, VueActive1 et VueActive2

--- LOG121_TP4/src/ets/VueActive1.java
+++ LOG121_TP4/src/ets/VueActive1.java
@@ -1,19 +1,70 @@
 package ets;
 
+
+import java.awt.*;
 import java.util.Observable;
 import java.util.Observer;
+
 
 /**
  *
  *
  *          Historique des modifications
  ***************************************************
- * @author
- * 2013-11-
+ * @author Jacob Marcil
+ * 2013-11-26
  */
 public class VueActive1 implements Observer {
+
+    private Image imageVueActive1;
+    private Perspective perspective;
+    private int hauteur;
+    private int largeur;
+
     @Override
     public void update(Observable observable, Object o) {
         //To change body of implemented methods use File | Settings | File Templates.
+        ImageConcrete image = (ImageConcrete) observable;
+        this.setVueActive1(image.getTheImage());
     }
+
+    /**
+     * Constructeur par default de VueActive1
+     */
+    public VueActive1(){}
+
+    /**
+     * Constructeur
+     * @param image
+     * @param largeur
+     * @param hauteur
+     */
+    public VueActive1(Image image, int largeur, int hauteur){
+        this.imageVueActive1 = image;
+        this.hauteur = hauteur;
+        this.largeur = largeur;
+    }
+
+    /**
+     * Setter de la vueActive1
+     * @param image
+     */
+    public void setVueActive1(Image image){
+        this.imageVueActive1 = image;
+        this.hauteur = image.getHeight(null);
+        this.largeur = image.getWidth(null);
+    }
+
+    /**
+     * Permet de dessiner plein écran la vue active selon la perspective
+     * @param g
+     */
+    public void paintComponent(Graphics g){
+        g.drawImage(imageVueActive1, (int)perspective.getCornerImage().getX(), (int)perspective.getCornerImage().getY(),
+                (int)(perspective.getCornerImage().getX()+largeur)*perspective.getEchelle(),
+                (int)(perspective.getCornerImage().getY()+hauteur)*perspective.getEchelle(), 0, 0 , largeur, hauteur, null);
+
+    }
+
+
 }
--- LOG121_TP4/src/ets/VueActive2.java
+++ LOG121_TP4/src/ets/VueActive2.java
@@ -1,19 +1,70 @@
 package ets;
 
+
+import java.awt.*;
 import java.util.Observable;
 import java.util.Observer;
 
-/**
- * Created with IntelliJ IDEA.
- * User: lucie
- * Date: 2013-11-19
- * Time: 20:00
- * To change this template use File | Settings | File Templates.
- */
 
+/**
+ *
+ *
+ *          Historique des modifications
+ ***************************************************
+ * @author Jacob Marcil
+ * 2013-11-26
+ */
 public class VueActive2 implements Observer {
+
+    private Image imageVueActive2;
+    private Perspective perspective;
+    private int hauteur;
+    private int largeur;
+
     @Override
     public void update(Observable observable, Object o) {
         //To change body of implemented methods use File | Settings | File Templates.
+        ImageConcrete image = (ImageConcrete) observable;
+        this.setVueActive2(image.getTheImage());
     }
+
+    /**
+     * Constructeur par default
+     */
+    public VueActive2(){}
+
+    /**
+     * Constructeur
+     * @param image
+     * @param largeur
+     * @param hauteur
+     */
+    public VueActive2(Image image, int largeur, int hauteur){
+        this.imageVueActive2 = image;
+        this.hauteur = hauteur;
+        this.largeur = largeur;
+    }
+
+    /**
+     * Setter de vueActive2
+     * @param image
+     */
+    public void setVueActive2(Image image){
+        this.imageVueActive2 = image;
+        this.hauteur = image.getHeight(null);
+        this.largeur = image.getWidth(null);
+    }
+
+    /**
+     * Permet le dessin de vueActive2 selon la perspective
+     * @param g
+     */
+    public void paintComponent(Graphics g){
+        g.drawImage(imageVueActive2, (int)perspective.getCornerImage().getX(), (int)perspective.getCornerImage().getY(),
+                (int)(perspective.getCornerImage().getX()+largeur)*perspective.getEchelle(),
+                (int)(perspective.getCornerImage().getY()+hauteur)*perspective.getEchelle(), 0, 0 , largeur, hauteur, null);
+
+    }
+
+
 }
