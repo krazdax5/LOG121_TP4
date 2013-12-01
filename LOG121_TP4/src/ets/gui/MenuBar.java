59dc7d8e9d898599dc9feaeb4aff0406474b7af7
Jacob Marcil <jacobmarcil2@gmail.com>
2013-12-01 00:35:45 -0500

    Bouton deplacer

--- LOG121_TP4/src/ets/gui/MenuBar.java
+++ LOG121_TP4/src/ets/gui/MenuBar.java
@@ -169,10 +169,23 @@ public class MenuBar extends JMenuBar {
         deplacer.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-//                Controlleur.getControlleur().deplacer(PanneauPrincipal.getPanneauPrincipal().vueActive.getPerspectiveVueActive1(0,0));
+                JTextField translationHorizontale = new JTextField();
+                JTextField translationVerticale = new JTextField();
+                Object[] message = {
+                        "Translation Horizontale:", translationHorizontale,
+                        "Translation Verticale:", translationVerticale
+                };
+
+                int translation = JOptionPane.showConfirmDialog(null, message, "Translation", JOptionPane.OK_CANCEL_OPTION);
+                if(translation == JOptionPane.OK_OPTION){
+                    Controlleur.getControlleur().deplacer(PanneauPrincipal.getPanneauPrincipal().vueActive.getPerspectiveVueActive1(),
+                            Integer.parseInt(translationHorizontale.getText()), Integer.parseInt(translationVerticale.getText()));
+
+                }
+
+
             }
         });
-
         menuVue.add(deplacer);
         //**************Fin deplacer ******************
 
